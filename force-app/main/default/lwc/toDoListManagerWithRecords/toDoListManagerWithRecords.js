37239e757f69b444a44716aace618bc065badc79
Premkumar-KP <kp.prem1002@gmail.com>
2023-12-15 20:28:01 +0530

    m-"Final Code"

--- force-app/main/default/lwc/toDoListManagerWithRecords/toDoListManagerWithRecords.js
+++ force-app/main/default/lwc/toDoListManagerWithRecords/toDoListManagerWithRecords.js
@@ -1,10 +1,53 @@
-import { LightningElement } from 'lwc';
+import { createRecord, deleteRecord, updateRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import TASK_MANAGER_OBJECT from '@salesforce/schema/Task_Manager__c';
+import TASK_ID from '@salesforce/schema/Task_Manager__c.Id';
+import TASK_NAME from '@salesforce/schema/Task_Manager__c.Name';
+import TASK_START_DATE from '@salesforce/schema/Task_Manager__c.Start_Date__c';
+import TASK_COMPLETED_DATE from '@salesforce/schema/Task_Manager__c.Completed_Date__c';
+import TASK_IS_COMPLETED from '@salesforce/schema/Task_Manager__c.Is_Completed__c';
+import GETINCOMPLETEDRECORDS from '@salesforce/apex/TaskManagerController.getIncompletedTask';
+import GETCOMPLETEDRECORDS from '@salesforce/apex/TaskManagerController.getcompletedTask';
+import { refreshApex } from '@salesforce/apex'
+import { LightningElement, wire } from 'lwc';
 
 export default class ToDoListManagerWithRecords extends LightningElement {
     taskName = "";
     taskDate = null;
     inCompeltedTask = [];
     compeltedTask = [];
+    inCompeltedTaskResponse = {};
+    compeltedTaskResponse = {};
+
+    @wire(GETINCOMPLETEDRECORDS)
+    wireGetIncompletedTask(response) {
+        this.inCompeltedTaskResponse = response;
+        let { data, error } = response;
+        if (data) {
+            this.inCompeltedTask = data.map((currentItem) => ({
+                taskId: currentItem.Id,
+                taskname: currentItem.Name,
+                taskdate: currentItem.Start_Date__c
+            }))
+        } else if (error) {
+            console.log('ErrorCompletedTask' + error);
+        }
+    }
+
+    @wire(GETCOMPLETEDRECORDS)
+    wireGetCompletedTask(response) {
+        this.compeltedTaskResponse = response;
+        let { data, error } = response;
+        if (data) {
+            this.compeltedTask = data.map((currentItem) => ({
+                taskId: currentItem.Id,
+                taskname: currentItem.Name,
+                taskdate: currentItem.Start_Date__c
+            }))
+        } else if (error) {
+            console.log('ErrorCompletedTask' + error);
+        }
+    }
 
     onChangeHandler(event) {
         let { name, value } = event.target;
@@ -13,8 +56,6 @@ export default class ToDoListManagerWithRecords extends LightningElement {
         } if (name === 'taskdate') {
             this.taskDate = value;
         }
-        console.log(this.taskName);
-        console.log(this.taskDate);
     }
     resetHandler() {
         this.taskName = "";
@@ -26,26 +67,34 @@ export default class ToDoListManagerWithRecords extends LightningElement {
             this.taskDate = new Date().toISOString().slice(0, 10);
         }
         if (this.validateTask()) {
-            this.inCompeltedTask = [...this.inCompeltedTask, {
-                taskname: this.taskName,
-                taskdate: this.taskDate
-            }];
-            this.resetHandler();
-            console.log('BeforeSortingInCompletedTask' + this.inCompeltedTask);
-            let sortedArray = this.sortArray(this.inCompeltedTask);
-            this.inCompeltedTask = [...sortedArray];
-            console.log('InCompletedTask' + this.inCompeltedTask);
-        }
 
+            let inputFields = {};
+            inputFields[TASK_NAME.fieldApiName] = this.taskName;
+            inputFields[TASK_START_DATE.fieldApiName] = this.taskDate;
+            inputFields[TASK_IS_COMPLETED.fieldApiName] = false;
+
+            let recordInput = {
+                apiName: TASK_MANAGER_OBJECT.objectApiName,
+                fields: inputFields
+            };
+
+            createRecord(recordInput)
+                .then((result) => {
+                    this.showToast("Success", "Task Created Successfully", "success")
+                    this.resetHandler();
+                    refreshApex(this.inCompeltedTaskResponse);
+                })
+                .catch((error) => {
+                    this.showToast("Failed", "Unable To Create Task", "error")
+                })
+        }
     }
 
     validateTask() {
         let isValid = true;
         let element = this.template.querySelector(".taskname");
-
         // condition 1 -- check the task name is empty
         // condition 2 -- check the task name is repeted 
-
         if (!this.taskName) { // task name is empty
             isValid = false;
         } else {
@@ -60,7 +109,6 @@ export default class ToDoListManagerWithRecords extends LightningElement {
                 element.setCustomValidity("Task already exist");
             }
         }
-
         if (isValid) {
             element.setCustomValidity("");
         }
@@ -68,30 +116,26 @@ export default class ToDoListManagerWithRecords extends LightningElement {
         return isValid;
     }
 
-    sortArray(inputArray) {
-        let sortbydate = inputArray.sort((a, b) => {
-            const date1 = new Date(a.taskdate);
-            const date2 = new Date(b.taskdate);
-            return date1 - date2;
-        });
-        return sortbydate;
-    }
-
     deleteTaskHandler(event) {
-        let index = event.target.name;
-        this.inCompeltedTask.splice(index, 1);
-        let sortedArray = this.sortArray(this.inCompeltedTask);
-        this.inCompeltedTask = [...sortedArray];
-        console.log('InCompletedTask' + this.inCompeltedTask);
+        let recordId = event.target.name;
+
+        deleteRecord(recordId)
+            .then((result) => {
+                this.showToast("Deleted", "Task Deleted Successfully", "success")
+                refreshApex(this.inCompeltedTaskResponse);
+            })
+            .catch((error) => {
+                this.showToast("Failed", "Unable To Delete Task", "error")
+            })
     }
 
     completeTaskHandler(event) {
-        let index = event.target.name;
-        this.refresData(index);
+        let recordId = event.target.name;
+        this.refresData(recordId);
     }
 
     dragHandler(event) {
-        event.dataTransfer.setData("index", event.target.dataset.item);
+        event.dataTransfer.setData("recordId", event.target.dataset.item);
     }
 
     allowDropHandler(event) {
@@ -99,14 +143,38 @@ export default class ToDoListManagerWithRecords extends LightningElement {
     }
 
     onDropHandler(event) {
-        let index = event.dataTransfer.getData("index");
-        this.refresData(index);
+        let recordId = event.dataTransfer.getData("recordId");
+        this.refresData(recordId);
     }
 
-    refresData(index) {
-        let removedItem = this.inCompeltedTask.splice(index, 1);
-        let sortedArray = this.sortArray(this.inCompeltedTask);
-        this.inCompeltedTask = [...sortedArray];
-        this.compeltedTask = [...this.compeltedTask, removedItem[0]];
+    async refresData(recordId) {
+
+        let inputFields = {};
+        inputFields[TASK_ID.fieldApiName] = recordId;
+        inputFields[TASK_IS_COMPLETED.fieldApiName] = true;
+        inputFields[TASK_COMPLETED_DATE.fieldApiName] = new Date().toISOString().slice(0, 10);
+
+        let recordInput = {
+            fields: inputFields
+        };
+        try {
+            await updateRecord(recordInput);
+            this.showToast("Updated", "Task Updated Successfully", "success");
+            await refreshApex(this.inCompeltedTaskResponse);
+            await refreshApex(this.compeltedTaskResponse);
+        }
+        catch (error) {
+            console.log('Error While updating a record', JSON.stringify(error));
+            this.showToast("Failed", "Unable To Update Task", "error")
+        }
+    }
+
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(event);
     }
 }
